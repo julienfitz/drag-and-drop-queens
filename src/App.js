a5cbb82315ebb28738b004304c33bb5c5ddc0ba9
Julien Fitzpatrick <jfitzpatrick@artemisag.com>
2020-08-22 20:36:43 -0700

    Initial commit - drag (and drop) queens!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
+  const onDragStart = (dragEvent) => {
+    // I added a border at this point so I can clearly see what's being dragged
+    dragEvent.currentTarget.style.border = '1px solid pink'
+    dragEvent.dataTransfer.setData('text/plain', dragEvent.target.id)
+  }
+
+  const onDragEnter = (dragEvent) => {
+    dragEvent.preventDefault()
+  }  
+
+  const onDragOver = (dragEvent) => {
+    dragEvent.preventDefault()
+    dragEvent.target.style.border = '1px solid green'
+  }
+
+  const onDrop = (dragEvent) => {
+    dragEvent.preventDefault()
+    const data = dragEvent.dataTransfer.getData('text')
+    const newElement = document.getElementById(data)
+    dragEvent.target.appendChild(newElement)
+    dragEvent.target.style.border = 'none'
+    newElement.style.border = 'none'
+    dragEvent.dataTransfer.clearData()
+  }
+
+  const Queen = ({ name }) => (
+    <li
+      draggable='true'
+      // also added a unique id so the list item can be "found"
+      id={`source-${name.split(' ').join('-')}`}
+      onDragStart={onDragStart}
+    >
+      {name}
+    </li>
+  )
+
+  const QueensList = () => {
+    const queens = [
+      'Divine',
+      'Lady Bunny',
+      'Sasha Velour',
+      'Vaginal Creme Davis',
+      'The Fabulous Wonder Twins'
+    ]
+
+    return (
+      <ul>
+        {
+          queens.map((queenName) => (
+            <Queen name={queenName} />
+          ))
+        }
+      </ul>
+    )
+  }
+
+  const FavoriteQueens = () => (
+    <ul
+      id='target'
+      onDragEnter={onDragEnter}
+      onDragOver={onDragOver}
+      onDrop={onDrop}
+    >
+      <li>Drop Target</li>
+    </ul>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <QueensList />
+      <FavoriteQueens />
+    </>
+  )
 }
 
-export default App;
+export default App
